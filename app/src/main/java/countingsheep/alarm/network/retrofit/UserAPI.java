0131d203e7a058bc545515f3714305fc30668c3b
ferencz8 <ferenczv8@gmail.com>
2019-07-19 20:21:38 +0300

    Update UserAPI.java

--- app/src/main/java/countingsheep/alarm/network/retrofit/UserAPI.java
+++ app/src/main/java/countingsheep/alarm/network/retrofit/UserAPI.java
@@ -5,15 +5,18 @@ import countingsheep.alarm.core.domain.UserRegistration;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface UserAPI {
 
 
     @POST("/alarm/api/user/register")
-    //@POST("/api/user/register")
+    //@POST("/alarm/api/user/register")
     Call<UserRegistration> register(@Body User user);
 
+    @Headers({"Accept: application/json"})
     @GET("/alarm/api/user/GetCredits")
-    Call<Integer> getFreeCredits(int userId);
+    Call<Integer> getFreeCredits(@Query("id")int userId);
 }
